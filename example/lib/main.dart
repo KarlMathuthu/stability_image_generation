71a1378fb0974c83d5d017a084c355892fe4b480
Karlmathuthu <karlmathuthu11@gmail.com>
2023-08-18 16:25:26 +0200

    commit

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,125 +1,177 @@
-import 'package:flutter/material.dart';
+/// Importing the 'dart:io' package to detect the platform, the 'brain_fusion' package for AI processing, the 'flutter/foundation.dart' package for debugging, and the 'flutter/material.dart' package for widget building.import 'dart:io' show Platform;
+// ignore_for_file: prefer_const_constructors
 
+import 'dart:io' show Platform;
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:stability_image_generation/stability_image_generation.dart';
+
+/// The [main] function is the entry point of the application
 void main() {
+  /// Call the runApp function to start the app
   runApp(const MyApp());
 }
 
+/// The [MyApp] widget is the root widget of the app
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+
+      /// Set the app theme to use Material 3
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+
+      /// Set the app home page to be the Test widget
+      home: const Test(title: 'Example'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+/// The [Test] widget.
+class Test extends StatefulWidget {
+  /// The title of the widget.
   final String title;
 
+  const Test({Key? key, required this.title}) : super(key: key);
+
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<Test> createState() => _TestState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+/// The [_TestState] widget.
+class _TestState extends State<Test> {
+  /// The text editing controller for the query.
+  final TextEditingController _queryController = TextEditingController();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  /// Initializes the [AI] class from the 'brain_fusion' package.
+  final StabilityAI _ai = StabilityAI();
+
+  /// The boolean value to run the function.
+  bool run = false;
+
+  /// The [_generate] function to generate image data.
+  Future<Uint8List> _generate(String query) async {
+    /// Call the runAI method with the required parameters.
+    Uint8List image = await _ai.generateImage(
+    
+    );
+    return image;
+  }
+
+  @override
+  void dispose() {
+    /// Dispose the [_queryController].
+    _queryController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    /// The size of the container for the generated image.
+    final double size = Platform.isAndroid || Platform.isIOS
+        ? MediaQuery.of(context).size.width
+        : MediaQuery.of(context).size.height / 2;
     return Scaffold(
+      backgroundColor: Colors.grey.shade200,
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Container(
+              width: double.infinity,
+              height: 50,
+              margin: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                color: Colors.white,
+                border: Border.all(
+                  width: 0.5,
+                  color: Colors.grey.shade300,
+                ),
+              ),
+              child: TextField(
+                controller: _queryController,
+                decoration: InputDecoration(
+                  hintText: 'Enter query text...',
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.only(left: 8),
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: SizedBox(
+                height: size,
+                width: size,
+                child: run
+                    ? FutureBuilder<Uint8List>(
+                        /// Call the [_generate] function to get the image data.
+                        future: _generate(_queryController.text),
+                        builder: (context, snapshot) {
+                          if (snapshot.connectionState ==
+                              ConnectionState.waiting) {
+                            /// While waiting for the image data, display a loading indicator.
+                            return Center(
+                              child: const CircularProgressIndicator(),
+                            );
+                          } else if (snapshot.hasError) {
+                            /// If an error occurred while getting the image data, display an error message.
+                            return Text('Error: ${snapshot.error}');
+                          } else if (snapshot.hasData) {
+                            /// If the image data is available, display the image using Image.memory().
+                            return ClipRRect(
+                              borderRadius: BorderRadius.circular(10),
+                              child: Image.memory(snapshot.data!),
+                            );
+                          } else {
+                            /// If no data is available, display a placeholder or an empty container.
+                            return Container();
+                          }
+                        },
+                      )
+                    : const Center(
+                        child: Text(
+                          'Enter Text and Click the button to generate',
+                          style: TextStyle(
+                            fontStyle: FontStyle.italic,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+              ),
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: () {
+          /// Get the user input from the [_queryController].
+          String query = _queryController.text;
+          if (query.isNotEmpty) {
+            /// If the user input is not empty, set [run] to true to generate the image.
+            setState(() {
+              run = true;
+            });
+          } else {
+            /// If the user input is empty, print an error message.
+            if (kDebugMode) {
+              print('Query is empty !!');
+            }
+          }
+        },
+        tooltip: 'Generate',
+        child: const Icon(Icons.gesture),
+      ),
     );
   }
 }
